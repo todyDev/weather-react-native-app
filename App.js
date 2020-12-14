aabcd9e8b21a60c23c9a650c8be8098072bf104c
Doyun <y0unn@hanmail.net>
2020-12-14 22:19:20 +0900

    :alembic: layout test with flexbox

--- App.js
+++ App.js
@@ -5,7 +5,8 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.yellowView}></View>
+      <View style={styles.blueView}></View>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +15,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
+    backgroundColor: "#fff"
+  },
+  yellowView: {
+    flex: 1,
+    backgroundColor: "yellow"
+  },
+  blueView: {
+    flex: 4,
+    backgroundColor: "blue"
   }
 });
